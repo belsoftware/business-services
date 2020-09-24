006ecf9d99d6a3a9aab9ab126e54c864ce2f6185
vipin <vipinpublicdomain@gmail.com>
2020-09-24 13:31:31 +0530

    Sync with egov product commits

--- billing-service/src/main/java/org/egov/demand/service/UserService.java
+++ billing-service/src/main/java/org/egov/demand/service/UserService.java
@@ -1,10 +1,11 @@
 package org.egov.demand.service;
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.egov.common.contract.request.RequestInfo;
 import org.egov.demand.config.ApplicationProperties;
@@ -21,13 +22,13 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class UserService {
-
+	
     @Autowired
     private RestTemplate restTemplate; 
-
+    
     @Autowired
     private ApplicationProperties properties;
-
+	
     /**
      * Fetched user based on phone number.
      * Note: Currently all CITIZEN are state-level and hence the phone no (which is set as username) is unique across state. 
@@ -36,41 +37,44 @@ public class UserService {
      * @param phoneNo
      * @return
      */
-	public Map<String, String> getUser(RequestInfo requestInfo, String phoneNo, String tenantId) {
-
+	public Map<String, String> getUser(RequestInfo requestInfo, String phoneNo, String name, String tenantId) {
+		
 		Map<String, Object> request = new HashMap<>();
 		UserResponse userResponse = null;
 		Map<String, String> response = new HashMap<>();
 		request.put("RequestInfo", requestInfo);
 		request.put("userName", phoneNo);
 		request.put("type", "CITIZEN");
+		request.put("name", name);
 		request.put("tenantId", tenantId.split("\\.")[0]);
-
+		
 		StringBuilder url = new StringBuilder();
 		url.append(properties.getUserServiceHostName()).append(properties.getUserServiceSearchPath());
-
+		
 		try {
-
+			
 			userResponse = restTemplate.postForObject(url.toString(), request, UserResponse.class);
 			if (null != userResponse) {
-				if (!CollectionUtils.isEmpty(userResponse.getUser()))
+				if (!CollectionUtils.isEmpty(userResponse.getUser())) {
+
 					response.put("id", userResponse.getUser().get(0).getUuid());
+				}
 			}
 		} catch (Exception e) {
 			log.error("Exception while fetching user: ", e);
 		}
-
+		
 		return response;
 	}
-
+	
 	public String createUser(Demand demand, RequestInfo requestInfo) {
-
+		
 		User payer = demand.getPayer();
-
+		
 		Map<String, Object> request = new HashMap<>();
 		Map<String, Object> user = new HashMap<>();
 		Map<String, String> role = new HashMap<>();
-
+		
 		List<Map<String, String>> roles = new ArrayList<>();
 		role.put("code", "CITIZEN");
 		role.put("name", "Citizen");
@@ -79,7 +83,7 @@ public class UserService {
 
 		user.put("name", payer.getName());
 		user.put("mobileNumber", payer.getMobileNumber());
-		user.put("userName", payer.getMobileNumber());
+		user.put("userName", UUID.randomUUID().toString());
 		user.put("active", true);
 		user.put("type", "CITIZEN");
 		user.put("tenantId", demand.getTenantId().split("\\.")[0]);
@@ -91,9 +95,9 @@ public class UserService {
 		UserResponse response = null;
 		StringBuilder url = new StringBuilder();
 		url.append(properties.getUserServiceHostName()).append(properties.getUserCreateEnpoint());
-
+		
 		try {
-
+			
 			response = restTemplate.postForObject(url.toString(), request, UserResponse.class);
 		}catch(Exception e) {
 			log.error("Exception while creating user: ", e);
@@ -104,3 +108,4 @@ public class UserService {
 	}
 
 }
+
