b46fa2f976dd999719ad6ab119427e00b3eeca54
santhups52 <santhups52@gmail.com>
2021-04-30 10:57:21 +0530

    ORS Service modified and included endpoint added 11

--- dss-sync/src/main/java/org/bel/dsssync/cronjob/HourlyJob.java
+++ dss-sync/src/main/java/org/bel/dsssync/cronjob/HourlyJob.java
@@ -167,7 +167,7 @@ public class HourlyJob implements Job {
 		try {
 			HttpHeaders headers = new HttpHeaders();
 			headers.set("TOKEN", token);
-			headers.setContentType(MediaType.TEXT_PLAIN);
+			headers.setContentType(MediaType.APPLICATION_JSON);
 			headers.setAccept(Arrays.asList(MediaType.ALL));
 			headers.set("Accept-Encoding","*");
 			HttpEntity<Object> entity = new HttpEntity<>(request, headers);
